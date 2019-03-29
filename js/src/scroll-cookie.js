ebc304e634e5897e36a7fd24e6762236cecd3237
h-hx <hhx14@qq.com>
2019-03-29 13:43:23 +0800

    Site updated: 2019-03-29 13:43:21

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 27 2019 09:47:41 GMT+0800 (GMT+08:00)
+// build time:Fri Mar 29 2019 13:43:20 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
