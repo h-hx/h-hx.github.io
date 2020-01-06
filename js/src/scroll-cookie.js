177f85828ca71168fa9f1b04111576ff59cd5707
h-hx <hhx14@qq.com>
2020-01-06 08:31:57 +0800

    Site updated: 2020-01-06 08:31:53

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 09 2019 16:20:20 GMT+0800 (GMT+08:00)
+// build time:Mon Jan 06 2020 08:31:52 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
